7a4301cefec8a6181ceab21059018c5d2cafc1ce
Diego <diego.lopez.19@hotmail.es>
2022-03-22 19:35:40 -0600

    init:new-project

---
+++ vite-react-app/src/main.jsx
@@ -0,0 +1,10 @@
+import {StrictMode} from 'react'
+import {render} from 'react-dom'
+import './index.css'
+
+ReactDOM.render(
+  <StrictMode>
+   <p>Vite app</p>
+  </StrictMode>,
+  document.getElementById('root')
+)
